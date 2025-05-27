74fcfcdb5642c430d17b8faee76df4d5bd1c5a78
Z1ted <hailongkkl321@gmail.com>
2025-05-27 10:27:40 +0700

    Trang đăng ký, đăng nhập

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/signup', function () {
+    return view('register');
+});
+Route::get('/login', function () {
+    return view('login');
+});
