2b3919e9d173609a7ab1f76cf9634e6e4b5cf870
KhoaEban <khoaebanypk03641@gmail.com>
2025-05-29 16:20:20 +0700

    ( commit )

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -1,9 +1,9 @@
-
 <?php
 
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
 
 class CartController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// trang chủ
 Route::get('/', function () {
     return view('welcome');
 });
 
+// trang đăng ký seller
 Route::get('/seller/register', function () {
     return view('seller.register.register');
 });
@@ -20,8 +22,8 @@ Route::get('/seller/register2', function () {
 Route::get('/seller/register3', function () {
     return view('seller.register.register3');
 });
-<<<<<<< Updated upstream
 
+// trang 404
 Route::get('/404', function () {
     return view('error.404NotFound');
 });
@@ -34,20 +36,20 @@ Route::get('/client/checkout', function () {
     return view('client.checkout');
 });
 
+// trang đăng ký và đăng nhập
 Route::get('/signup', function () {
     return view('register');
 });
 Route::get('/login', function () {
     return view('login');
 });
-=======
+
+// trang lịch sử đơn hàng
 Route::get('/user/order/order-history', function () {
     return view('user.order.order_history');
 }); 
 
+// trang giỏ hàng
 Route::get('/user/cart', function () {
     return view('user.cart');
 });
-
-
->>>>>>> Stashed changes
