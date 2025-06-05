c1dae7918c8d3e102fa86f6177b2c5b909e7bb1d
Z1ted <hailongkkl321@gmail.com>
2025-06-05 15:15:25 +0700

    Cập nhật thông tin người dùng , thêm xóa sửa địa chỉ giao hàng

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Auth;
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserAddressController;
 ///////////////////////////////////////////////////////////
 // trang chủ
 Route::get('/', function () {
