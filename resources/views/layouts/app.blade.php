b4c051bcb9d157eb4169e8c1a0fa2c6ba9404fc8
KhoaEban <khoaebanypk03641@gmail.com>
2025-07-12 14:49:55 +0700

    ( commit )

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png" />
-    <title>@yield('title')</title>
-
+    <title>@yield('title', 'Default Title')</title>
+    
     <!-- Font + Tailwind + Icons -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
